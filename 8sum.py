1e80fefdea5a4949f877ebf1a92db96d2937ea5a
Naminenisravani <41769909+Naminenisravani@users.noreply.github.com>
2018-08-01 17:40:00 +0530

    Rename number.py to 8sum.py

---
+++ 8sum.py
@@ -0,0 +1,11 @@
+n=int(raw_input())
+num = int(input("enter the value of n: "))
+hold = num
+sum = 0
+f num<= 0:
+   print("enter a whole positive number!")
+else:
+   while num > 0:
+         sum = sum + num
+         num = num - 1;
+   print("sum of first",hold,"natural numbers is: ",sum)     
