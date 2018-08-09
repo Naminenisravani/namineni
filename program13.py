5bb68424da515dfde7e47d5e12883a15f1981160
Naminenisravani <41769909+Naminenisravani@users.noreply.github.com>
2018-08-09 14:22:40 +0530

    Update program13.py

--- program13.py
+++ program13.py
@@ -1,14 +1,11 @@
 n=int(raw_input())
-number = int(input("Enter any number "))
-
-# prime number is always greater than 1
-if number > 1:
-    for i in range(2, number):
-        if (number % i) == 0:
-            print(number, "is not a prime number")
+if n> 1:
+    for i in range(2, n):
+        if (n % i) == 0:
+            print("Not a prime number")
             break
     else:
-        print(number, "is a prime number")
+        print( "Prime number")
 
 else:
     print(number, "is not a prime number")
