ab3e461ad3fc1846e5cd3f0328a68ae2a87416fb
Naminenisravani <41769909+Naminenisravani@users.noreply.github.com>
2018-08-16 10:26:26 +0530

    Update program25.py

--- program25.py
+++ program25.py
@@ -5,5 +5,5 @@ def median(l,n):
   else:
     return (l[n/2-1]+l[n/2])/2
   n=int(raw_input())
-  l=[int(p) for p in raw_input ().split()]
+  l=[int(a) for a in raw_input ().split()]
   print(median(l,n-1))
