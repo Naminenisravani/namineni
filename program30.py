9055a55ef42f15b205853ac624062b94ee98504b
Naminenisravani <41769909+Naminenisravani@users.noreply.github.com>
2018-08-16 12:26:49 +0530

    Update program30.py

--- program30.py
+++ program30.py
@@ -1,8 +1,8 @@
 hour1,min1=map(int,raw_input().split())
 hour2,min2=map(int,raw_input().split())
-m1=hour1*60+min1
-m2=hour2*60+min2
-diff=abs(m1-m2)
+t1=hour1*60+min1
+t2=hour2*60+min2
+diff=abs(t1-t2)
 time=diff%60
 time1=(diff-time)//60
 print time1,time
